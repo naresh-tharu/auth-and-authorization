45d177660df359fea16cc500f43f50a1f0517348
Naresh Tharu <jr.er.nareshtharu@gmail.com>
2024-04-21 21:12:18 +0545

    auth routes defined

--- src/app/auth/auth.router.js
+++ src/app/auth/auth.router.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
 const router = Router();
 
+router.post('/register', (req, res, next) => { })
+router.post('/activate/:token', (req, res, next) => { })
+router.post('/login', (req, res, next) => { })
+router.get('/me', (req, res, next) => { })
+router.post('/forget-password', (req, res, next) => { })
+router.post('/set-password/:token', (req, res, next) => { })
 
 export default router;
